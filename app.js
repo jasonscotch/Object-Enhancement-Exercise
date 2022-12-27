b4ce9e37a1c8ddbd38b4c6aa55f9ffcb5a803ec0
jasonscotch <jason.m.scot@gmail.com>
2022-12-26 18:11:05 -0800

    upload ObjectEnhancementExercise

---
+++ app.js
@@ -0,0 +1,65 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+// Write an ES2015 Version
+
+const createInstructor = (firstName, lastName) => {
+    return {
+    firstName, 
+    lastName
+    };
+}
+
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+// Write an ES2015 Version
+
+const favoriteNumber = 42;
+
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: 'That is my favorite!'
+}
+
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+// Write an ES2015 Version
+
+const instructor2 = {
+    fistName: 'Colt',
+    sayHi() {
+        return 'Hi!';
+    },
+    sayBye() {
+        return this.firstName + ' says bye!';
+    }
+}
+
+const createAnimal = (species,verb,noise) => {
+   return {
+        species,
+        [verb](){
+            return noise;
+        }
+    };
+}
